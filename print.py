31f40ef90e3376952f84d6b25afa1362e035542e
MP Techno Solutions <41568630+devops-mptech@users.noreply.github.com>
2018-11-18 05:41:11 +0530

    Update print.py

--- print.py
+++ print.py
@@ -7,3 +7,8 @@ z=x+y
 print("sum of {} and {} is {}".format(y,x,z))
 print("sum of " +str(x)+" and " +str(y)+ " is " +str(z))
 print("His name is {} , age is {} and salary is {}".format(name,age,salary))
+
+#a="rama"
+# b=20.5
+# c=30
+# print("a value is %s, b value is %f and c value is %i"%(a,b,c))
